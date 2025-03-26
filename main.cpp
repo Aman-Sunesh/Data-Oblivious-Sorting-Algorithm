8469fd5a184749475efaf29030d684af992be378
Aman Sunesh <157351006+Aman-Sunesh@users.noreply.github.com>
2025-03-26 15:30:08 +0000

    Initial commit of all project files

--- main.cpp
+++ main.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 #include <algorithm>
-
 #include "secret.h"
 
 using namespace std;
 
 
-void secret_sort(std::vector<Secret> & arr)
+void secret_sort(std::vector<Secret> & arr) // Bubble Sort Algorithm
 {
-// ...
+    size_t n = arr.size();
+    Secret one(1); // Secret value representing 1 is created to be used for secret comparisons
+
+    for (size_t i = 0; i < n - 1; i++)
+    {
+        for (size_t j = 0; j < n - i - 1; j++)
+        {
+            // Compare adjacent elements in a secret way
+            Secret cond = arr[j+1] < arr[j];
+
+            // Swap if cond is secret 1, otherwise no change
+            Secret new_j = cond * arr[j+1] + (one - cond) * arr[j];
+            Secret new_j1 = cond * arr[j] + (one - cond) * arr[j+1];
+
+            // Assign new values to the array elements
+            arr[j] = new_j;
+            arr[j + 1] = new_j1;
+        }
+    }
 }
 
 void shuffle(std::vector<Secret> & arr)
@@ -29,6 +46,7 @@ void test()
     secret_sort(phrase);
     cout << decrypt(phrase) << '\n';
 }
+
 /* this function outputs:
 Some phrase
 ***********
